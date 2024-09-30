3592257e86da8f494b0308ba79e320e52b76e355
Christopher <146177013+krisnat102@users.noreply.github.com>
2024-09-30 12:16:06 +0300

    Update file 9/30/2024, 12:16:06 PM

--- 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.cs
+++ 28-FindtheIndexoftheFirstOccurrenceinaString/28-FindtheIndexoftheFirstOccurrenceinaString.cs
@@ -1,6 +1,5 @@
 public class Solution {
     public int StrStr(string haystack, string needle) {
-        if(!haystack.Contains(needle)) return -1;
         return haystack.IndexOf(needle);
     }
 }
